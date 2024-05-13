117126467e3e2b6793295d95511f3229d35716e0
AhmedAbdlhakeemSadawy <ahmed_abd_elhakeem@yahoo.com>
2024-05-13 21:59:05 +0300

    Add database context

---
+++ DataAccess/ECommerceDataAccess/DatabaseContextConfiguration/ECommerceDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ECommerceDataAccess.DataEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ECommerceDataAccess.DatabaseContextConfiguration
+{
+    internal class ECommerceDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Here you would configure your database connection, for example:
+                optionsBuilder.UseSqlServer("YourConnectionString");
+            }
+        }
+    }
+}
