70da55f72d8138e2d8c604f9964c36d1b914c467
AhmedAbdlhakeemSadawy <ahmed_abd_elhakeem@yahoo.com>
2024-07-27 23:34:09 +0300

    Remove unneeded configration

--- DataAccess/ECommerceDataAccess/DatabaseContextConfiguration/ECommerceDbContext.cs
+++ DataAccess/ECommerceDataAccess/DatabaseContextConfiguration/ECommerceDbContext.cs
@@ -13,30 +13,12 @@ namespace ECommerceDataAccess.DatabaseContextConfiguration
 {
     public class ECommerceDbContext : DbContext
     {
-        public virtual DbSet<Product> Products { get; set; }
 
-   
         public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options)
      : base(options)
         {
         }
-
-
-        public class ECommerceDbContextFactory : IDesignTimeDbContextFactory<ECommerceDbContext>
-        {
-            public ECommerceDbContext CreateDbContext(string[] args)
-            {
-                var optionsBuilder = new DbContextOptionsBuilder<ECommerceDbContext>();
-                optionsBuilder.UseSqlServer("Server=.;Initial Catalog=ECommerce;Trusted_Connection=True;Integrated Security=true;Encrypt=False");
-
-                return new ECommerceDbContext(optionsBuilder.Options);
-            }
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=.;Initial Catalog=ECommerce;Trusted_Connection=True;Integrated Security=true;Encrypt=False");
-        }
+        public virtual DbSet<Product> Products { get; set; }
 
 
     }
