beb6f6c5ac70840ff2a3401a46e34dce34a8b1c1
AhmedAbdlhakeemSadawy <ahmed_abd_elhakeem@yahoo.com>
2024-07-27 23:35:30 +0300

    Update migration

--- DataAccess/ECommerceDataAccess/Migrations/20240727202941_InitialCreate.Designer.cs
+++ DataAccess/ECommerceDataAccess/Migrations/20240727202941_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerceDataAccess.Migrations
 {
     [DbContext(typeof(ECommerceDbContext))]
-    [Migration("20240727192004_InitialCreate")]
+    [Migration("20240727202941_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -19,7 +19,7 @@ namespace ECommerceDataAccess.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.0")
+                .HasAnnotation("ProductVersion", "7.0.20")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
--- DataAccess/ECommerceDataAccess/Migrations/ECommerceDbContextModelSnapshot.cs
+++ DataAccess/ECommerceDataAccess/Migrations/ECommerceDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace ECommerceDataAccess.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.0")
+                .HasAnnotation("ProductVersion", "7.0.20")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
