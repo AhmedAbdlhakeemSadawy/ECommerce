209ec13f787487177b5184dfd9a9f7e03c7f8307
AhmedAbdlhakeemSadawy <ahmed_abd_elhakeem@yahoo.com>
2024-07-27 22:39:08 +0300

    Add web Api project to register ef core which is used in Data Access project

---
+++ WebAPI/ECommwerceWebAPI/Program.cs
@@ -0,0 +1,40 @@
+
+using ECommerceDataAccess.DatabaseContextConfiguration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+builder.Services.AddDbContext<ECommerceDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ECommerceConnection")));
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
