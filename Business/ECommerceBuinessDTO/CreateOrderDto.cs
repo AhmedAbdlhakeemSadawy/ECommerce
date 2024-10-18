c49b2712ce78a6da84a05f6e13caafe9788b684e
AhmedAbdlhakeemSadawy <ahmed_abd_elhakeem@yahoo.com>
2024-10-18 21:44:52 +0300

    Add needed DTOs

---
+++ Business/ECommerceBuinessDTO/CreateOrderDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBuinessDTO
+{
+    public class CreateOrderDto
+    {
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<ProductDTO> products { get; set; }
+    }
+}
---
+++ Business/ECommerceBuinessDTO/OrderDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceBuinessDTO
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int Status { get; set; }
+        public List<ProductDTO> products { get; set; }
+    }
+}
