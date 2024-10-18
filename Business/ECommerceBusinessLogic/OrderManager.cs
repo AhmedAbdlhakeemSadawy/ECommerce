f37e55d2eb91580620734eb43ed97b425f8bfb00
AhmedAbdlhakeemSadawy <ahmed_abd_elhakeem@yahoo.com>
2024-10-18 21:47:41 +0300

    Add create order implmenetation

---
+++ Business/ECommerceBusinessLogic/OrderManager.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceBuinessDTO;
+using ECommerceBusinessAbstractions;
+
+namespace ECommerceBusinessLogic
+{
+    public class OrderManager : IOrderManager
+    {
+        public OrderDTO CreateOrder(CreateOrderDto createOrderDto)
+        {
+            if (createOrderDto.products.Count == 0)
+            {
+                throw new Exception("Order Should contain al least one prodcut");
+            }
+            
+        }
+    }
+}
