7a27426590d56c27959462eb4bb534be0b63920a
AhmedAbdlhakeemSadawy <ahmed_abd_elhakeem@yahoo.com>
2024-10-30 21:23:21 +0300

    add migration to add stockQuantity coulmn to product table

--- DataAccess/ECommerceDataAccess/Migrations/20241030181540_add-stock-quantity-to-product-table.Designer.cs
+++ DataAccess/ECommerceDataAccess/Migrations/20241030181540_add-stock-quantity-to-product-table.Designer.cs
@@ -4,6 +4,7 @@ using ECommerceDataAccess.DatabaseContextConfiguration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerceDataAccess.Migrations
 {
     [DbContext(typeof(ECommerceDbContext))]
-    partial class ECommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241030181540_add-stock-quantity-to-product-table")]
+    partial class addstockquantitytoproducttable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccess/ECommerceDataAccess/Migrations/20241030181540_add-stock-quantity-to-product-table.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addstockquantitytoproducttable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "OrderProducts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockQuantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StockQuantity",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "OrderProducts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
