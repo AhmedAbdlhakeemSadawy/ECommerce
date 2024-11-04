baab0d76a2f406e59cb82dd702cae7c8cb5010d8
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2024-11-04 16:49:18 +0300

    Add unit test to calculate total price

--- Business/ECommerceBusinessTests/OrderManagerTests.cs
+++ Business/ECommerceBusinessTests/OrderManagerTests.cs
@@ -39,5 +39,29 @@ namespace ECommerceBusinessTests
             var exception = Assert.Throws<Exception>(() => orderManager.CreateOrder(createOrderDto));
             Assert.Equal("Some of your products are not available", exception.Message);
         }
+
+
+        [Fact]
+        public void CreateOrder_WithAvailableProductQuantity_CalculateTotalPrice()
+        {
+            var mockProductRepository = new Mock<IProductRepository>();
+            CreateOrderDto createOrderDto = new CreateOrderDto();
+
+            createOrderDto.products.Add(new ProductDTO { Id = 1, Name = "Product One", Quantiy = 1 });
+            createOrderDto.products.Add(new ProductDTO { Id = 2, Name = "Product Two", Quantiy = 1 });
+
+            mockProductRepository.Setup(repo => repo.GetProductStockQuantity(new List<int> { 1, 2 })).Returns(new List<productStockDTO>
+            { new productStockDTO() { Id = 1 ,StockQuantity = 2} ,new productStockDTO() { Id = 2 ,StockQuantity = 4}});
+
+
+            mockProductRepository.Setup(repo => repo.GetListProductsById(new List<int> { 1, 2 })).Returns(new List<ECommerceDataAccessDTO.ProductDto>
+            { new ECommerceDataAccessDTO.ProductDto() { Id = 1 ,Price = 120} ,new ECommerceDataAccessDTO.ProductDto() { Id = 2 ,Price = 80}});
+
+            OrderManager orderManager = new OrderManager(mockProductRepository.Object);
+
+            OrderDTO orderDTO = orderManager.CreateOrder(createOrderDto);
+
+            Assert.Equal(200, orderDTO.TotalPrice);
+        }
     }
 }
