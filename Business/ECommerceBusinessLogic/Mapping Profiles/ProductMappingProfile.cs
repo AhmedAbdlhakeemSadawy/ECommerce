2b7cfc42a789db47b112a71875fa9dfa067d9a40
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2024-11-24 15:52:49 +0300

    Add Automapper product profile

---
+++ Business/ECommerceBusinessLogic/Mapping Profiles/ProductMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ECommerceBuinessDTO;
+using ECommerceDataAccessDTO;
+
+namespace ECommerceBusinessLogic.Mapping_Profiles
+{
+    public class ProductMappingProfile : Profile
+    {
+        public ProductMappingProfile()
+        {
+           CreateMap<ProductBusinessDTO, ProductDataDto>();
+
+           CreateMap<ProductDataDto, ProductBusinessDTO>();
+
+        }
+
+
+
+    }
+}
