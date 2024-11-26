d124b268b18d7e8e208d65b477fec6b8aa1aff55
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2024-11-26 13:08:29 +0300

    Add new project for core contracts to make other libraries depend on these contracts

---
+++ ECommerceCoreContracts/ProductContract.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceCoreContracts
+{
+    public abstract class ProductContract
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
