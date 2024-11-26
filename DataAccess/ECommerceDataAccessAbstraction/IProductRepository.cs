28d5b41a2d09844729f2e40c6abdfe7f058ec52b
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2024-11-26 13:07:07 +0300

    Add new project for Data Access Abstraction contain Interfaces for data access

---
+++ DataAccess/ECommerceDataAccessAbstraction/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceCoreContracts;
+using ECommerceDataAccessDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceDataAccessAbstraction
+{
+    public interface IProductRepository<TProduct> : IRepository<TProduct> where TProduct : ProductContract
+    {
+        IEnumerable<ProductDataDto> GetListProductsById(List<int> ids);
+        IEnumerable<ProductDataDto> UpdateProductsStockQuantity(List<ProductDataDto> productDataDtos, List<ProductDataDto> productsDataDtosStockUpdated);
+    }
+}
