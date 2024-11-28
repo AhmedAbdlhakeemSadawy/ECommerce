bab28dd0be82e48847cb5fdd65405bfae3601e8f
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2024-11-28 17:35:58 +0300

    Add OrderDataDto as a DTO for DataAccess layer

---
+++ DataAccess/ECommerceDataAccessDTO/OrderDataDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceDataAccessDTO
+{
+    public class OrderDataDto
+    {
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int Status { get; set; }
+        public List<ProductDataDto> products { get; set; } = new List<ProductDataDto>();
+    }
+}
