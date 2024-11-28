884b4384fb929be5fb4fca9cdbc449e36ff9fd14
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2024-11-28 17:38:22 +0300

    Add IOrderRepository as  a abstraction for IOrderRepository

---
+++ DataAccess/ECommerceDataAccessAbstraction/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceDataAccessDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceDataAccessAbstraction
+{
+    public interface IOrderRepository<TOrderDataDto> : IRepository<TOrderDataDto> where TOrderDataDto : OrderDataDto
+    {
+    }
+}
