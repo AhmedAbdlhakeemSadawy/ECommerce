f6c3cd20ad63a82b6566810db2f9d2f50510be0e
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2024-12-01 17:10:32 +0300

    Adding order repository implementation

---
+++ DataAccess/ECommerceDataAccess/OrderRepository/OrderRepository.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using ECommerceDataAccess.DatabaseContextConfiguration;
+using ECommerceDataAccess.DataEntities;
+using ECommerceDataAccessAbstraction;
+using ECommerceDataAccessDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceDataAccess.OrderRepository
+{
+    public class OrderRepository : IOrderRepository<OrderDataDto>
+    {
+        private readonly ECommerceDbContext context;
+        private IMapper mapper;
+
+        public OrderRepository(ECommerceDbContext context,IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+        public Task AddAsync(OrderDataDto entity)
+        {
+            Order order  = mapper.Map<Order>(entity);
+            context.Add(order);
+            return context.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(OrderDataDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<OrderDataDto>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderDataDto> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(OrderDataDto entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
