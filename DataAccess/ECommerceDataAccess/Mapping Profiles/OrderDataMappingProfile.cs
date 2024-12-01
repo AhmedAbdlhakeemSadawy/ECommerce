bfb06481c672d3821d33768552326910f2a92496
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2024-12-01 17:09:56 +0300

    Adding mapping configration for order and product DTo and data entities

---
+++ DataAccess/ECommerceDataAccess/Mapping Profiles/OrderDataMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ECommerceDataAccess.DataEntities;
+using ECommerceDataAccessDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceDataAccess.Mapping_Profiles
+{
+    public class OrderDataMappingProfile : Profile
+    {
+        public OrderDataMappingProfile()
+        {
+            CreateMap<OrderDataDto, Order>();
+            CreateMap<Order, OrderDataDto>();
+        }
+    }
+}
