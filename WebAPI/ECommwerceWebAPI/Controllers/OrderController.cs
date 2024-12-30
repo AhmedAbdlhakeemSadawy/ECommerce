9dfff53ba941b03cd9c94a7627a0123a15078f2a
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2024-12-30 17:15:48 +0300

    Add Order Controller

---
+++ WebAPI/ECommwerceWebAPI/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceBusinessAbstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommwerceWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : Controller
+    {
+        IOrderManager orderManager;
+        public OrderController(IOrderManager orderManager)
+        {
+            this.orderManager = orderManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
