80d5d7fdf2072ef4bbdc9bd30a8b2123444a089d
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-01-06 16:35:34 +0300

    Add Request DTo Validators using Fluent Validation

---
+++ WebAPI/ECommerceWebApiDto/Validators/OrderRequestDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceWebApiDto.Validators
+{
+    public class OrderRequestDtoValidator : AbstractValidator<OrderRequestDto>
+    {
+        public OrderRequestDtoValidator()
+        {
+            RuleFor(orderRequestDto => orderRequestDto.CustomerId)
+             .GreaterThan(0)
+             .WithMessage("CustomerId is required and must be greater than 0.");
+
+            RuleFor(orderRequestDto => orderRequestDto.Products)
+                .NotNull()
+                .WithMessage("Products cannot be null.")
+                .NotEmpty()
+                .WithMessage("Products must contain at least one item.");
+
+            RuleForEach(order => order.Products).SetValidator(new ProductRequestDtoValidator());
+        }
+    }
+}
