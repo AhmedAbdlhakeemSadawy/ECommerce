330f9aa0b9664d97385aabb02ca98e06b13f7d18
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-01-08 17:28:08 +0300

    Implement Unit of Work pattern

---
+++ DataAccess/ECommerceDataAccessAbstraction/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceDataAccessDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceDataAccessAbstraction
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository<ProductDataDto> ProductRepository { get; }
+        IOrderRepository<OrderDataDto>  OrderRepository { get; }
+
+        public int Complete();
+
+    }
+}
