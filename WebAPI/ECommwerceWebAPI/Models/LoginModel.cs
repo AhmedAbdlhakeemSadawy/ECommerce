3e4610ab956528191a56c99196bfb389f9dad74d
AhmedAbdlhakeemSadawy <ahmed_abd_elhakeem@yahoo.com>
2025-01-14 22:32:38 +0300

    Add models for Account controller

---
+++ WebAPI/ECommwerceWebAPI/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommwerceWebAPI.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ WebAPI/ECommwerceWebAPI/Models/RegisterModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommwerceWebAPI.Models
+{
+    public class RegisterModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
