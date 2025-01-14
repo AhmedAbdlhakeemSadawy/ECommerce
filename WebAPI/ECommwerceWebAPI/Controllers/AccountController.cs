52540269ef75928acc75d6d401ecfbbf89d895aa
AhmedAbdlhakeemSadawy <ahmed_abd_elhakeem@yahoo.com>
2025-01-14 22:31:50 +0300

    Add Register and login API

---
+++ WebAPI/ECommwerceWebAPI/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using ECommwerceWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommwerceWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+                return Ok("User registered successfully.");
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpGet("accessdenied")]
+        public IActionResult AccessDenied()
+        {
+            return Forbid("You do not have access to this resource.");
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+                return Ok("Login successful.");
+
+            return Unauthorized("Invalid login attempt.");
+        }
+    }
+}
