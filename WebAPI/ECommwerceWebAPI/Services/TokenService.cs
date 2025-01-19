f4c66dcc06ffcbb5b55233a7e18affa0d56f08a7
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-01-19 16:59:58 +0300

    Implement generate token logic

---
+++ WebAPI/ECommwerceWebAPI/Services/TokenService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApiAbstraction;
+
+namespace ECommwerceWebAPI.Services
+{
+    public class TokenService : ITokenService
+    {
+
+        private readonly IConfiguration configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string GenerateToken(IEnumerable<Claim> claims)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(int.Parse(jwtSettings["DurationInMinutes"])),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
