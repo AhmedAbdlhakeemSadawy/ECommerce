b31fd7e26e163bcd314079ed794f4866f18f9850
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-01-21 16:35:58 +0300

    Make jwt as a default schema for authetication

--- WebAPI/ECommwerceWebAPI/Program.cs
+++ WebAPI/ECommwerceWebAPI/Program.cs
@@ -18,6 +18,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using WebApiAbstraction;
 
@@ -45,7 +46,7 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
 .AddDefaultTokenProviders();
 // Add services to the container.
 
-#region Cookie based athentication configration
+//#region Cookie based athentication configration
 //builder.Services.ConfigureApplicationCookie(options =>
 //{
 //    options.Cookie.HttpOnly = true;
@@ -70,10 +71,15 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
 //    //options.LogoutPath = "/Account/Logout"; // Redirect path for logout
 //    options.AccessDeniedPath = "/api/Account/accessdenied"; // Redirect for unauthorized access
 //});
-#endregion
+//#endregion
 
+#region configration for jwt based authentication
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
     .AddJwtBearer(options =>
     {
         options.TokenValidationParameters = new TokenValidationParameters
@@ -91,6 +97,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]))  // The signing key
         };
     });
+#endregion
 builder.Services.AddControllers();
 builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
 builder.Services.AddValidatorsFromAssemblyContaining<OrderRequestDtoValidator>();
@@ -108,6 +115,34 @@ builder.Services.AddSingleton(provider => new MapperConfiguration(cfg =>
 }).CreateMapper());
 builder.Services.AddScoped<IDataSeeder, DataSeeder>();
 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        BearerFormat = "JWT",
+        Scheme = "Bearer",
+        Description = "Enter 'Bearer' followed by a space and then your JWT token"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
 var app = builder.Build();
 
 
@@ -116,6 +151,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
