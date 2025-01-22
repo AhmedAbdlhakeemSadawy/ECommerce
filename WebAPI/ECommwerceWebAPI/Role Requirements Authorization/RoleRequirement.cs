f5c714d05642560ca2f4c33f48a72f36c793b32f
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-01-22 15:25:11 +0300

    Add role requirment autherization to handle logic if role included in token check it if not get it from database

---
+++ WebAPI/ECommwerceWebAPI/Role Requirements Authorization/RoleRequirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace ECommwerceWebAPI.Role_Requirements_Authorization
+{
+    public class RoleRequirement : IAuthorizationRequirement
+    {
+        public string RequiredRole { get; }
+
+        public RoleRequirement(string requiredRole)
+        {
+            RequiredRole = requiredRole;
+        }
+    }
+}
---
+++ WebAPI/ECommwerceWebAPI/Role Requirements Authorization/RoleRequirementHandler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using WebApiAbstraction.Role_Authuntication;
+
+namespace ECommwerceWebAPI.Role_Requirements_Authorization
+{
+    public class RoleRequirementHandler : AuthorizationHandler<RoleRequirement>
+    {
+        private readonly IUserRoleService userRoleService;
+
+        public RoleRequirementHandler(IUserRoleService userRoleService)
+        {
+            this.userRoleService = userRoleService;
+        }
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, RoleRequirement requirement)
+        {
+
+            if (!context.User.Identity?.IsAuthenticated == true)
+            {
+                return;
+            }
+
+            var userRoles = context.User.FindAll(ClaimTypes.Role).Select(c => c.Value).ToList();
+            if (userRoles.Contains(requirement.RequiredRole))
+            {
+                context.Succeed(requirement);  // Role found in the token
+                return;
+            }
+
+            var userId = context.User.FindFirst(JwtRegisteredClaimNames.Sid)?.Value;
+            if (userId != null)
+            {
+                var roles = await userRoleService.GetRolesForUserAsync(userId);
+                if (roles.Contains(requirement.RequiredRole))
+                {
+                    context.Succeed(requirement); // Role found in database
+                }
+            }
+
+        }
+    }
+}
