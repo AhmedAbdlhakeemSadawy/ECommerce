d41cad9c8e24f4c9be031038fdf18d157601a6a6
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-01-22 15:22:52 +0300

    Add User role service to get roles for specific user from database using ASP.Net core Identity

---
+++ WebAPI/ECommwerceWebAPI/Services/UserRoleService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebApiAbstraction.Role_Authuntication;
+
+namespace ECommwerceWebAPI.Services
+{
+    public class UserRoleService : IUserRoleService
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public UserRoleService(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public async Task<List<string>> GetRolesForUserAsync(string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return new List<string>(); // Return empty list if user is not found
+            }
+
+            return (await userManager.GetRolesAsync(user)).ToList();
+        }
+    }
+}
