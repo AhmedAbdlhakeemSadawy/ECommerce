1ee554d87434d2f7c4b3832fef62217cc2621ba0
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-06-17 17:09:51 +0300

    register token interceptor

--- ecommerce-ui/src/main.ts
+++ ecommerce-ui/src/main.ts
@@ -1,6 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { TokenInterceptor} from './app/services/token.interceptor';
+import { AuthService } from './app/services/auth.service';
 
-bootstrapApplication(AppComponent, appConfig)
+import { provideHttpClient, withInterceptorsFromDi  } from '@angular/common/http';
+
+const updatedAppConfig = {
+  providers: [
+    ...(appConfig.providers || []),
+      AuthService,
+      TokenInterceptor,
+      provideHttpClient(withInterceptorsFromDi())
+    ]
+};
+
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     AuthService,
+//     TokenInterceptor,
+//     provideHttpClient(
+//       withInterceptorsFromDi() // Must use this to enable DI
+//     )
+//   ]
+// }).catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, updatedAppConfig)
   .catch((err) => console.error(err));
