42c4ea2cde6094d8d96f6e893fc6564871f2d0d4
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-07-17 15:16:33 +0300

    Add Azure Communication service as a implementation Class for Email Serivce infrastructure

---
+++ ECommerceInfrastructure/AzureCommunicationEmailService.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceInfrastructureAbstraction;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualBasic.FileIO;
+using System.Runtime.CompilerServices;
+
+namespace ECommerceInfrastructure
+{
+    public class AzureCommunicationEmailService : IEmailService
+    {
+        private readonly AzureEmailCommunicationSettings azureEmailCommunicationSettings;
+
+        public AzureCommunicationEmailService(IOptions<AzureEmailCommunicationSettings> options)
+        {
+            azureEmailCommunicationSettings = options.Value;
+        }
+        public Task SendEmailAsync(string toEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
