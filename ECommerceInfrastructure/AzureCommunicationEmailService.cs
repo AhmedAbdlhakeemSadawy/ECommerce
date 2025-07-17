dfe260af5aa163c2819f9d799c4c72b54a1bfa56
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-07-17 17:29:23 +0300

    Add initial implmentation to send email

--- ECommerceInfrastructure/AzureCommunicationEmailService.cs
+++ ECommerceInfrastructure/AzureCommunicationEmailService.cs
@@ -1,6 +1,10 @@
-﻿using ECommerceInfrastructureAbstraction;
+﻿using Azure;
+using Azure.Communication.Email;
+using Azure.Identity;
+using ECommerceInfrastructureAbstraction;
 using Microsoft.Extensions.Options;
 using Microsoft.VisualBasic.FileIO;
+using System.Net;
 using System.Runtime.CompilerServices;
 
 namespace ECommerceInfrastructure
@@ -9,13 +13,20 @@ namespace ECommerceInfrastructure
     {
         private readonly AzureEmailCommunicationSettings azureEmailCommunicationSettings;
 
-        public AzureCommunicationEmailService(IOptions<AzureEmailCommunicationSettings> options)
+        private readonly EmailClient emailClient;
+        public AzureCommunicationEmailService()
         {
-            azureEmailCommunicationSettings = options.Value;
+            emailClient = new EmailClient(new Uri(""), new DefaultAzureCredential());
         }
-        public Task SendEmailAsync(string toEmail)
+
+        public async Task SendEmailAsync(string toEmail)
         {
-            throw new NotImplementedException();
+            var emailContent = new EmailContent($"Order Confirmation - Order ")
+            {
+                PlainText = $"Thank you for your order! Your order  has been received.",
+                Html = $"<h1>Order Confirmation</h1><pTaking you for your order! Your order  has been received.</p>"
+            };
+            var sendResult = await emailClient.SendAsync(WaitUntil.Completed,new EmailMessage( "fromEmail", toEmail, emailContent));
         }
     }
 }
