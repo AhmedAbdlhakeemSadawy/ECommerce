f8adc8ae0f439b7ff6ae3cb73e4818411e9774a7
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-07-21 14:58:09 +0300

    Send Email using Azure after get configration from Enviroment Variables

--- ECommerceInfrastructure/AzureCommunicationEmailService.cs
+++ ECommerceInfrastructure/AzureCommunicationEmailService.cs
@@ -1,9 +1,11 @@
 ﻿using Azure;
 using Azure.Communication.Email;
+using Azure.Core.Diagnostics;
 using Azure.Identity;
 using ECommerceInfrastructureAbstraction;
 using Microsoft.Extensions.Options;
 using Microsoft.VisualBasic.FileIO;
+using System.Diagnostics.Tracing;
 using System.Net;
 using System.Runtime.CompilerServices;
 
@@ -14,19 +16,36 @@ namespace ECommerceInfrastructure
         private readonly AzureEmailCommunicationSettings azureEmailCommunicationSettings;
 
         private readonly EmailClient emailClient;
-        public AzureCommunicationEmailService()
+        public AzureCommunicationEmailService(IOptions<AzureEmailCommunicationSettings> options)
         {
-            emailClient = new EmailClient(new Uri(""), new DefaultAzureCredential());
+            azureEmailCommunicationSettings = options.Value;
         }
 
         public async Task SendEmailAsync(string toEmail)
         {
+            var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+            {
+                ExcludeEnvironmentCredential = true,
+                ExcludeManagedIdentityCredential = true,
+                ExcludeSharedTokenCacheCredential = true,
+                //ExcludeAzureCliCredential = true,
+                ExcludeInteractiveBrowserCredential = true,
+                ExcludeAzurePowerShellCredential = true,
+                ExcludeVisualStudioCredential = true,
+            });
+
+            
+           // AzureEventSourceListener listener = AzureEventSourceListener.CreateConsoleLogger(EventLevel.Verbose);
+
+
+            var emailClient = new EmailClient(new Uri(azureEmailCommunicationSettings.UrI), credential);
+
             var emailContent = new EmailContent($"Order Confirmation - Order ")
             {
                 PlainText = $"Thank you for your order! Your order  has been received.",
                 Html = $"<h1>Order Confirmation</h1><pTaking you for your order! Your order  has been received.</p>"
             };
-            var sendResult = await emailClient.SendAsync(WaitUntil.Completed,new EmailMessage( "fromEmail", toEmail, emailContent));
+            var sendResult = await emailClient.SendAsync(WaitUntil.Completed,new EmailMessage(azureEmailCommunicationSettings.EmailFrom, toEmail, emailContent));
         }
     }
 }
