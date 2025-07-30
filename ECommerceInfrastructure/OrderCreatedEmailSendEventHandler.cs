4730c4f666b2437f113a5e098cbf274a3bd24ecd
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-07-30 16:21:14 +0300

    Add Order Created Email Event Handler to send email using email service

---
+++ ECommerceInfrastructure/OrderCreatedEmailSendEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using ECommerceBusinessAbstractions;
+using ECommerceEvents;
+using ECommerceInfrastructureAbstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceInfrastructure
+{
+    public class OrderCreatedEmailSendEventHandler : IDomainEventHandler<OrderCreatedEvent>
+    {
+        private readonly IEmailService emailService;
+        public OrderCreatedEmailSendEventHandler(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+        public async Task Handle(OrderCreatedEvent orderCreatedEvent)
+        {
+            string content = "$\"<h1>Order Confirmation</h1><p> Kindly note that Your order number {orderCreatedEvent.Id}  has been received.</p>\"";
+            await emailService.SendEmailAsync(orderCreatedEvent.CustomerEmail, "Order Confirmation", content);
+            throw new NotImplementedException();
+        }
+    }
+}
