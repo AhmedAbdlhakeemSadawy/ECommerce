e2faf536fc174319c916ef108a81286a48ffca6e
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-07-30 16:19:34 +0300

    Add implementation for Event bus as InMemory since call handlers once published from business

---
+++ ECommerceInfrastructure/InMemoryEventBus.cs
@@ -0,0 +1,44 @@
+﻿using ECommerceBusinessAbstractions;
+using ECommerceInfrastructureAbstraction;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceInfrastructure
+{
+    public class InMemoryEventBus : IEventBus
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public InMemoryEventBus(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+        public async void Publish(IDomainEvent domainEvent)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var handlers = scope.ServiceProvider.GetServices<IDomainEventHandler<IDomainEvent>>();
+            foreach (var handler in handlers)
+            {
+                await handler.Handle(domainEvent);
+            }
+        }
+
+        public void Subscribe<T, TH>()
+            where T : IDomainEvent
+            where TH : IDomainEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe<T, TH>()
+            where T : IDomainEvent
+            where TH : IDomainEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
