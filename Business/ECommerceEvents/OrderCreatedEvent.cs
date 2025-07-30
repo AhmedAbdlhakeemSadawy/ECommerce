b512561e1a2905ac6ac582d0225b69fca624db14
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-07-30 16:12:42 +0300

    Add ECommerce Business Events to include Events data

---
+++ Business/ECommerceEvents/OrderCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceBusinessAbstractions;
+
+namespace ECommerceEvents
+{
+    public class OrderCreatedEvent : IDomainEvent
+    {
+        public int Id { get; }
+        public decimal TotalPrice { get; }
+        public string CustomerEmail { get; }
+        public OrderCreatedEvent(int id, decimal totalPrice)
+        {
+            Id = id;
+            TotalPrice = totalPrice;
+        }
+    }
+}
