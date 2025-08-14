6a1d68b034d8a2bfc0d2792967ec594f1a8d7733
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-08-14 18:38:25 +0300

    Add Order Number to OrderCreatedEvent

--- Business/ECommerceEvents/OrderCreatedEvent.cs
+++ Business/ECommerceEvents/OrderCreatedEvent.cs
@@ -5,11 +5,13 @@ namespace ECommerceEvents
     public class OrderCreatedEvent : IDomainEvent
     {
         public int Id { get; }
+        public long OrderNumber { get; }
         public decimal TotalPrice { get; }
         public string CustomerEmail { get; }
-        public OrderCreatedEvent(int id, decimal totalPrice,string customerEmail)
+        public OrderCreatedEvent(int id, long orderNumber,decimal totalPrice,string customerEmail)
         {
             Id = id;
+            OrderNumber = orderNumber;
             TotalPrice = totalPrice;
             CustomerEmail = customerEmail;
         }
