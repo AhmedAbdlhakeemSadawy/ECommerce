6e8cb869077350f052e15f46c88f0d572b3a23d3
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-08-14 18:39:05 +0300

    Add Order number in OrderDataDto

--- DataAccess/ECommerceDataAccessDTO/OrderDataDto.cs
+++ DataAccess/ECommerceDataAccessDTO/OrderDataDto.cs
@@ -9,6 +9,7 @@ namespace ECommerceDataAccessDTO
     public class OrderDataDto
     {
         public int Id { get; set; }
+        public long OrderNumber { get; set; }
         public decimal TotalPrice { get; set; }
         public int Status { get; set; }
         public List<ProductOrderDataDto> products { get; set; } = new List<ProductOrderDataDto>();
