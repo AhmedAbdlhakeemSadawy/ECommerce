e66ddf74de447683aec667bb03dac2f893d5d7fb
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-08-20 17:54:23 +0300

    Fix Order Mapping

--- Business/ECommerceBusinessLogic/Mapping Profiles/OrderMappingProfile.cs
+++ Business/ECommerceBusinessLogic/Mapping Profiles/OrderMappingProfile.cs
@@ -18,6 +18,19 @@ namespace ECommerceBusinessLogic.Mapping_Profiles
             CreateMap<OrderDataDto, OrderBusinessDTO>()
              .ForMember(dest => dest.Status, opt => opt.MapFrom(src => (int)src.Status));
 
+           CreateMap<ProductBusinessDTO, ProductOrderDataDto>()
+            .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Id))  // Map Id to ProductId
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))  // Map Name to ProductName
+            .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity));
+
+            CreateMap<OrderBusinessDTO, OrderDataDto>()
+               .ForMember(dest => dest.Status, opt => opt.MapFrom(src => (int)src.Status));
+
+            CreateMap<ProductOrderDataDto, ProductBusinessDTO>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ProductId))  // Map Id to ProductId
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))  // Map Name to ProductName
+            .ForMember(dest => dest.StockQuantity, opt => opt.MapFrom(src => src.Quantity))
+            .ForMember(dest => dest.Quantity, opt => opt.UseDestinationValue());
         }
     }
 }
