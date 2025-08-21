df7cc38da5e9197aaa70823c4d198760e0e4c1cb
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-08-21 15:28:30 +0300

    Modify Update stock quantity to calculate the new value in buinsess layer and DataAccess layer just update

--- Business/ECommerceBusinessLogic/OrderManager.cs
+++ Business/ECommerceBusinessLogic/OrderManager.cs
@@ -59,7 +59,7 @@ namespace ECommerceBusinessLogic
 
             OrderDataDto orderDataDto = mapper.Map<OrderDataDto>(orderBusinessDto);
             await unitOfWork.OrderRepository.AddOrder(orderDataDto);
-            orderBusinessDto.products = UpdateProductsStockQuantities(orderBusinessDto.products, reterivedProdcutsBusinessDto);
+            orderBusinessDto.products = UpdateProductsStockQuantities(orderBusinessDto.products);
 
             await unitOfWork.Complete();
 
@@ -101,12 +101,19 @@ namespace ECommerceBusinessLogic
             return totalPrice;
         }
 
-        private List<ProductBusinessDTO> UpdateProductsStockQuantities(List<ProductBusinessDTO> productsBusinessNeedToUpdateStockDto, List<ProductBusinessDTO> retreviedProductsBusinessStock)
+        private List<ProductBusinessDTO> UpdateProductsStockQuantities(List<ProductBusinessDTO> productsBusinessNeedToUpdateStockDto)
         {
-            List<ProductDataDto> productsDataNeedToUpdateStockDto = mapper.Map<List<ProductDataDto>>(productsBusinessNeedToUpdateStockDto);
-            List<ProductDataDto> retreviedProductsDataStock = mapper.Map<List<ProductDataDto>>(retreviedProductsBusinessStock);
+            List<ProductDataDto> productDataDtosUpdatedStocks = new List<ProductDataDto>();
 
-            List<ProductDataDto> productDataDtosResult = unitOfWork.ProductRepository.UpdateProductsStockQuantity(productsDataNeedToUpdateStockDto, retreviedProductsDataStock).ToList();
+            mapper.Map(productsBusinessNeedToUpdateStockDto, productDataDtosUpdatedStocks);
+
+            for (int i = 0; i < productsBusinessNeedToUpdateStockDto.Count; i++)
+            {
+                productDataDtosUpdatedStocks.Where(p => p.Id == productsBusinessNeedToUpdateStockDto[i].Id).FirstOrDefault().StockQuantity = productsBusinessNeedToUpdateStockDto[i].StockQuantity - productsBusinessNeedToUpdateStockDto[i].Quantity;
+            }
+
+
+            List<ProductDataDto> productDataDtosResult = unitOfWork.ProductRepository.UpdateProductsStockQuantity(productDataDtosUpdatedStocks).ToList();
 
             List<ProductBusinessDTO> productsUpdatedResult = mapper.Map<List<ProductBusinessDTO>>(productDataDtosResult);
 
