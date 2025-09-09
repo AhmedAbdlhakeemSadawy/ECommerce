308e504f72ac68e0bb9d61486551911f8bbb35be
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-09-09 16:41:41 +0300

    Add enviroment production for angular

--- ecommerce-ui/src/environments/environment.prod.ts
+++ ecommerce-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: '/api' // Relative path for Azure (same domain)
+    apiUrl: 'https://ecommerceapplication-bya2fghah5ffaph0.uaenorth-01.azurewebsites.net/api' // Relative path for Azure (same domain)
   };
