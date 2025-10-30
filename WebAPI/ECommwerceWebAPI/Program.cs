f18ad5a0b26efd1bbf7b79bb4a90a8ac3b40d5a4
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-10-30 14:04:04 +0300

    Enable Swagger in production

--- WebAPI/ECommwerceWebAPI/Program.cs
+++ WebAPI/ECommwerceWebAPI/Program.cs
@@ -230,12 +230,8 @@ var app = builder.Build();
 
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
 app.UseCors("AllowAngularApp");
