6081cad9ec69a8266eb7a91f6d29a54fecc4a65f
AhmedAbdlhakeemSadawy <ahmedabdelhakeem33@gmail.com>
2025-12-30 10:17:49 +0300

    Register Azure Queue Service instead of InMemory

--- WebAPI/ECommwerceWebAPI/Program.cs
+++ WebAPI/ECommwerceWebAPI/Program.cs
@@ -158,7 +158,8 @@ builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsi
 builder.Services.AddValidatorsFromAssemblyContaining<OrderRequestDtoValidator>();
 builder.Services.AddScoped<AccessTokenValidationMiddleware>();
 builder.Services.Configure<AzureEmailCommunicationSettings>(builder.Configuration.GetSection("AzureEmailCommunicationSettings"));
-builder.Services.AddSingleton<IEventBus, InMemoryEventBus>();
+//builder.Services.AddSingleton<IEventBus, InMemoryEventBus>();
+builder.Services.AddSingleton<IEventBus, AzureQueueService>();
 
 builder.Services.AddScoped<IDomainEventHandler<OrderCreatedEvent>, OrderCreatedEmailSendEventHandler>();
 
