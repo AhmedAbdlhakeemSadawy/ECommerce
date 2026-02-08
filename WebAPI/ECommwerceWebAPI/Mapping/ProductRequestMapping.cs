b104f2e594310351b39cc0645e44b4dfa22b8ac0
AhmedAbdlhakeemSadawy <ahmed_abd_elhakeem@yahoo.com>
2026-02-08 16:04:30 +0300

    Add manual mapping for Request Dtos in Web API layer

---
+++ WebAPI/ECommwerceWebAPI/Mapping/ProductRequestMapping.cs
@@ -0,0 +1,70 @@
+﻿using ECommerceBuinessDTO;
+using ECommerceWebApiDto;
+
+namespace ECommwerceWebAPI.Mapping
+{
+    public static class ProductRequestMapping
+    {
+        public static ProductBusinessDTO ToBusinessDto(this ProductRequestDto request)
+        {
+            if (request == null) return null;
+
+            return new ProductBusinessDTO
+            {
+                Id = request.Id,
+                Quantity = request.Quantity,
+
+                // Will be filled later (DB / service layer)
+                Name = string.Empty,
+                Price = 0m,
+                StockQuantity = 0
+            };
+        }
+
+
+        public static ProductRequestDto ToRequestDto(this ProductBusinessDTO business)
+        {
+            if (business == null) return null;
+
+            return new ProductRequestDto
+            {
+                Id = business.Id,
+                Quantity = business.Quantity
+            };
+        }
+
+
+        public static List<ProductRequestDto> ToRequestDtos( this IEnumerable<ProductBusinessDTO> businessDtos)
+        {
+            if (businessDtos == null)
+                return new List<ProductRequestDto>();
+
+            var list = new List<ProductRequestDto>();
+            foreach (var b in businessDtos)
+                list.Add(b.ToRequestDto());
+
+            return list;
+        }
+
+        public static List<ProductBusinessDTO> ToBusinessDtos(this IEnumerable<ProductRequestDto> requests)
+        {
+            if (requests == null)
+                return new List<ProductBusinessDTO>();
+
+            if (requests is ICollection<ProductRequestDto> col)
+            {
+                var result = new List<ProductBusinessDTO>(col.Count);
+                foreach (var r in col)
+                    result.Add(r.ToBusinessDto());
+
+                return result;
+            }
+
+            var list = new List<ProductBusinessDTO>();
+            foreach (var r in requests)
+                list.Add(r.ToBusinessDto());
+
+            return list;
+        }
+    }
+}
